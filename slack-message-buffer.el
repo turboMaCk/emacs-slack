ff762d3e6c7e6264c94202ea0f7f7318c834e47b
Yuya Minami <yuya373@me.com>
2018-02-19 17:48:02 +0900

    should use latest instead of last-read when load missing message

--- slack-message-buffer.el
+++ slack-message-buffer.el
@@ -123,6 +123,7 @@
 
       (slack-buffer-insert-load-more this)
 
+      ;; TODO use latest instead of last-read
       (with-slots (room team last-read) this
         (let* ((messages (slack-room-sorted-messages room))
                (latest-message (car (last messages)))
@@ -245,6 +246,7 @@
     (oset this oldest (oref message ts))))
 
 (defmethod slack-buffer-load-missing-messages ((this slack-message-buffer))
+  ;; TODO use latest instead of last-read
   (with-slots (room team last-read) this
     (cl-labels
         ((request-messages (latest)
@@ -268,8 +270,9 @@
                                 (cl-loop for m in messages
                                          do (slack-buffer-insert this m t)))
                               (when latest-message
-                                (slack-buffer-update-last-read this
-                                                               latest-message))
+                                ;; (slack-buffer-update-last-read this
+                                ;;                                latest-message)
+                                )
                               (when oldest-message
                                 (slack-buffer-update-oldest this
                                                             oldest-message)))))))
